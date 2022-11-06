a4eccb82bf97ef59b6e916949e4454d16e6cc50b
OfirGrinberg <78484186+OfirGrinberg@users.noreply.github.com>
2022-11-06 19:38:35 +0200

    End of section 5

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
-// import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
     TooltipModule.forRoot(),
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent],
